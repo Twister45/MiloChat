1e39e93fa5c22dfbc4b89e85e792e1445160c5d3
Jessie Vela <jessie.vela45@hotmail.com>
2017-04-26 11:08:53 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+class Meme_Chat:
+
+    def __init__(self, master):
+        self.master = master
+        self.size = (500, 500)
+        master.title("Milo The Meme Chat")
+        self.image = Image.open("C:\\Users\\jessie_twister\\PycharmProjects\\MemeChat\\Pic\\milo.jpg")
+        self.image = self.image.resize(self.size)
+        self.photo = ImageTk.PhotoImage(self.image)
+        self.information()
+        self.grid()
+
+    def information(self):
+        self.app_title = Label(root, text="Milo The ... Chat", font=("Times", "20"))
+        self.input_chat_text = Label(root, text="Say something to 'Milo'", font=("Times", "16"))
+        self.input_chat_box = Entry(root)
+        self.input_chat_box.bind('<Return>', self.response)
+        self.chat_enter_button = Button(root, text=" Enter ", anchor=E)
+        self.chat_enter_button.bind('<Button-1>', self.response)
+        self.ImageResult = Label(root, width=500, height=500, image=self.photo)
+
+    def grid(self):
+        self.app_title.grid(row=0, columnspan=3)
+        self.ImageResult.grid(row=1, column=2, padx=30)
+        self.input_chat_text.grid(row=2, column=2)
+        self.input_chat_box.grid(row=3, column=2, pady=(10,15))
+        self.chat_enter_button.grid(row=3, column=2, sticky=E, padx=(0,150), pady=(10,15))
+
+    def response(self, event):
+        self.response = self.input_chat_box.get()
+        self.input_chat_box.delete(0, END)
+        api_key = "&api_key=dc6zaTOxFJmzC"
+        search_url = ("http://api.giphy.com/v1/gifs/search?q=" + self.response + api_key)
+        self.image2 = Image.open("C:\\Users\\jessie_twister\\PycharmProjects\\MemeChat\\Pic\\Sponge.gif")
+        self.image2 = self.image2.resize(self.size)
+        self.photo = ImageTk.PhotoImage(self.image2)
+        self.ImageResult.config(image=self.photo)
+        print(search_url)
+
+
+root = Tk()
+my_gui = Meme_Chat(root)
+root.mainloop()
